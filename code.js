2f406f4d4be29689eaf0b749df90e078b910394a
Tomas Gaitan <tomasgaitan.contact@gmail.com>
2024-02-17 17:53:40 -0300

    JavaScript function to display and collapse answer containers

---
+++ code.js
@@ -0,0 +1,21 @@
+let containers = document.querySelectorAll(".question-title-container");
+
+containers.forEach(container => {
+    container.addEventListener("click", function() {
+        let article = container.closest("article");
+        let answer = article.querySelector(".answer-container");
+        let allAnswers = document.querySelectorAll(".answer-container");
+        let img =  article.querySelector(".question-svg");
+        if (answer.classList.contains("open")){
+            answer.classList.replace("open", "close");
+            img.setAttribute("src", "images/icon-plus.svg");
+        }
+        else{
+            allAnswers.forEach(answer => {
+                answer.classList.replace("open", "close");
+            });
+            answer.classList.replace("close", "open");
+            img.setAttribute("src", "images/icon-minus.svg");
+        };
+    });
+});
